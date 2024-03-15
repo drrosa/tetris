421c27f700d9d959b5af096a1865bbd2174d2a7d
David Rosa <rosatamsen@gmail.com>
2024-03-15 15:17:38 -0600

    app: Cache HTML element references.

--- main.js
+++ main.js
@@ -5,6 +5,10 @@
 
 
         /*----- cached element references -----*/
+let canvasEl = document.getElementById("board-canvas");
+let spanEl = document.getElementById("score");
+let smallCanvasEl = document.getElementById("next-piece-canvas");
+let h2El = document.getElementById("message");
 
 
         /*----- event listeners -----*/
