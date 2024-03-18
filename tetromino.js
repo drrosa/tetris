db700bb15f8f315081aa04d13e79b49ce8e1f506
David Rosa <rosatamsen@gmail.com>
2024-03-18 13:02:54 -0600

    tetromino: Simplify object initialization to tighten encapsulation.

--- tetromino.js
+++ tetromino.js
@@ -52,24 +52,25 @@ export default class Tetromino {
 
   #y;
 
-  constructor() {
-    this.#spawn(Tetromino.#SHAPES, Tetromino.#COLORS);
-  }
+  #shape;
 
-  #spawn(shapes, colors) {
-    const idx = Math.floor(Math.random() * shapes.length);
-    this.shape = shapes[idx];
-    this.color = colors[idx];
-    const isMid = idx === 0 || idx === 6;
-    this.#x = isMid ? 3 : 1;
+  #color;
+
+  constructor() {
+    const idx = Math.floor(Math.random() * Tetromino.#SHAPES.length);
+    this.#shape = Tetromino.#SHAPES[idx];
+    this.#color = Tetromino.#COLORS[idx];
+    // The ⎢ and □ tetrominoes spawn centrally;
+    // the rest spawn in the left-middle columns.
+    this.#x = idx === 0 || idx === 6 ? 3 : 1;
     this.#y = 0;
   }
 
   render(canvasCtx) {
-    this.shape.forEach((row, j) => {
+    this.#shape.forEach((row, j) => {
       row.forEach((block, i) => {
         // eslint-disable-next-line no-param-reassign
-        canvasCtx.fillStyle = block ? this.color : '#15171b';
+        canvasCtx.fillStyle = block ? this.#color : '#15171b';
         canvasCtx.fillRect(this.#x + i, this.#y + j, 1, 1);
       });
     });
