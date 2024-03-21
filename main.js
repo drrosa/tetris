91117005c730a4b3ef58f1701627b9f15e6bcd95
David Rosa <rosatamsen@gmail.com>
2024-03-21 12:22:51 -0600

    app: Prevent starting a new game if one is already in progress.

--- main.js
+++ main.js
@@ -14,21 +14,22 @@ const msgEl = document.getElementById('message');
 /* ----- functions -----*/
 
 function init() {
-  const boardDisplay = boardEl.getContext('2d', { willReadFrequently: true });
-  const nextPieceDisplay = nextPiecelEl.getContext('2d');
+  if (!game) {
+    const boardDisplay = boardEl.getContext('2d', { willReadFrequently: true });
+    const nextPieceDisplay = nextPiecelEl.getContext('2d');
 
-  boardEl.width = WIDTH;
-  boardEl.height = HEIGHT;
-  boardDisplay.scale(BLOCK_SIZE, BLOCK_SIZE);
+    boardEl.width = WIDTH;
+    boardEl.height = HEIGHT;
+    boardDisplay.scale(BLOCK_SIZE, BLOCK_SIZE);
 
-  nextPiecelEl.width = HEIGHT;
-  nextPiecelEl.height = WIDTH;
-  nextPieceDisplay.scale(HEIGHT / 6, WIDTH / 6);
+    nextPiecelEl.width = HEIGHT;
+    nextPiecelEl.height = WIDTH;
+    nextPieceDisplay.scale(HEIGHT / 6, WIDTH / 6);
 
-  game = new TetrisGame(boardDisplay, scoreEl, nextPieceDisplay, msgEl);
-  game.start();
+    game = new TetrisGame(boardDisplay, scoreEl, nextPieceDisplay, msgEl);
+    game.start();
+  }
 }
 
 /* ----- event listeners -----*/
-
 document.getElementById('start-button').addEventListener('click', init);
